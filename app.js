ff856da9b28190cdbeaf00f0f4dcf2d6d7c816e7
Sean Stapleton <seanstapleton100@gmail.com>
2017-03-25 22:27:50 -0400

    basic intents added

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 var restify = require('restify');
 var builder = require('botbuilder');
+var moment = require('moment');
+
+//model from LUIS api
+var model = "https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/1cfc40cb-8baf-425b-b199-5d4064adbab1?subscription-key=138e101b1c524ed2bbbcd05fd2ae56ba&verbose=true&timezoneOffset=0.0&q=";
+var recognizer = new builder.LuisRecognizer(model);
+var intents = new builder.IntentDialog({ recognizers: [recognizer] });
+
 
 // Symptom storage
 var symptomList = [];
-var symptomIndex = 0;
-var newSymptom = function(symptom) {
-  symptomList[symptomIndex] = symptom;
-  symptomIndex++;
-};
+
+var m = moment();
+var timeOfDay = (m.hour() < 12) ? "morning" : (m.hour() < 5) ? "afternoon" : "evening";
 
 //=========================================================
 // Bot Setup
@@ -24,64 +29,104 @@ var connector = new builder.ChatConnector({
     appId: process.env.MICROSOFT_APP_ID,
     appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
-var bot = new builder.UniversalBot(connector);
+var bot = new builder.UniversalBot(connector, {persistConversationData: true});
 server.post('/api/messages', connector.listen());
 
-var intents = new builder.IntentDialog();
 //=========================================================
 // Bots Dialogs
 //=========================================================
 
-bot.dialog('/', intents);
-intents.matches(/^No/i, [
-    function (session) {
-        session.beginDialog('/makeDiagnosis');
-    },
-    function (session, results) {
-        session.send('Ok... Let me see what is wrong with you. ');
-    }
+bot.dialog('/collectData', intents);
+
+bot.dialog('/', [function(session) {
+  builder.Prompts.text(session, "Good " + timeOfDay + "! I am Doc. What is your name?");
+}, function(session, results) {
+  session.conversationData.name = results.response;
+  builder.Prompts.text(session, "Nice to meet you " + session.conversationData.name + "! How are you feeling this " + timeOfDay + "?");
+  session.beginDialog('collectData');
+}]);
+
+
+
+//=========================================================
+// Intent Handlers
+//=========================================================
+// dialog.matches('CollectSymptom', builder.DialogAction.send('Recognized symptom: '));
+intents.onDefault(builder.DialogAction.send("I'm sorry I didn't understand."));
+
+intents.matches('CollectSymptom', [
+  function(session, args, next) {
+    //resolve/store entities passed from LUIS
+    var title = builder.EntityRecognizer.findEntity(args.entities, 'Symptom');
+    builder.Prompts.text(session, "Recognized symptom: " + title.entity);
+  }
 ]);
 
-intents.onDefault([
-    function (session) {
-      session.beginDialog('/ensureProfile', session.userData.profile);
-    },
-    function (session, results) {
-      session.userData.profile = results.response;
-      session.send("Hi %(name)s! ", session.userData.profile);
-    },
-    function (session) {
-      session.beginDialog('/symptomAnalysis', session.userData.profile);
-    }
-]);
+// bot.dialog('/introduction', function(session) {
+//   builder.Prompts.text(session, "Good " + timeOfDay + "! I am Doc. What is your name?");
+// })
 
-bot.dialog('/ensureProfile', [
-    function (session, args, next) {
-      session.dialogData.profile = args || {};
-      if (!session.dialogData.profile.name) {
-          builder.Prompts.text(session, "Hi there! What's your name?");
-      } else {
-          next();
-      }
-    },
-    function (session, results, next) {
-      if (results.response) {
-          session.dialogData.profile.name = results.response;
-          session.send("Hi %(name)s! ", session.userData.profile);
-      }
-      session.endDialogWithResult(
-      { response: session.dialogData.profile });
-    }
-]);
+bot.dialog('/queryPatient', function(session) {
+  builder.Promps.text(session, "Have you any other problems?");
+}, function(session, results) {
+  var neg = results.match(/(^|\s+)no(\s+|$)/);
+  neg = neg.filter(v=>v!=' ');
+  if (neg.length > 0) {
+    bot.dialog('makeDiagnosis');
+    builder.Promps.text(session, "Diagnosing symptoms...");
+  }
+});
 
-bot.dialog('/symptomAnalysis', [
-    function (session, args, next) {
-      builder.Prompts.text(session, "How are you feeling?");
-    },
-    function (session, results, next) {
-      newSymptom(results.response);
-      session.endDialogWithResult(
-      { response: session.dialogData.profile });
-    }
 
-]);
+// intents.matches(/^No/i, [
+//     function (session) {
+//         session.beginDialog('/makeDiagnosis');
+//     },
+//     function (session, results) {
+//         session.send('Ok... Let me see what is wrong with you. ');
+//     }
+// ]);
+//
+// intents.onDefault([
+//     function (session) {
+//       session.beginDialog('/ensureProfile', session.conversationData.profile);
+//     },
+//     function (session, results) {
+//       session.conversationData.profile = results.response;
+//       session.send("Hi %(name)s! ", session.conversationData.profile);
+//     },
+//     function (session) {
+//       session.beginDialog('/symptomAnalysis', session.conversationData.profile);
+//     }
+// ]);
+//
+// bot.dialog('/ensureProfile', [
+//     function (session, args, next) {
+//       session.dialogData.profile = args || {};
+//       if (!session.dialogData.profile.name) {
+//           builder.Prompts.text(session, "Hi there! What&#39;s your name?");
+//       } else {
+//           next();
+//       }
+//     },
+//     function (session, results, next) {
+//       if (results.response) {
+//           session.dialogData.profile.name = results.response;
+//           session.send("Hi %(name)s! ", session.conversationData.profile);
+//       }
+//       session.endDialogWithResult(
+//       { response: session.dialogData.profile });
+//     }
+// ]);
+//
+// bot.dialog('/symptomAnalysis', [
+//     function (session, args, next) {
+//       builder.Prompts.text(session, "How are you feeling?");
+//     },
+//     function (session, results, next) {
+//       symptomList.push(results.response);
+//       session.endDialogWithResult(
+//       { response: session.dialogData.profile });
+//     }
+//
+// ]);
